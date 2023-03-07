cef3c4e4457558a1ddc71b22b1b3b91fcba5d6df
Sahil <leviack629@gmail.com>
2023-03-07 23:28:03 +0530

    numeric data types

---
+++ numeric.py
@@ -0,0 +1,16 @@
+#numeric data types
+
+
+num1 = 2
+print(num1)  #2
+print(type(num1))  #int
+
+num2 = 2.5
+print(num2)  #2.5
+print(type(num2))  #float
+
+num3 = 1+2j
+print(num3)  #1+2j
+print(type(num3))  #complex
+
+
