d4891e3cdc81679efff73c42de248366c865c9cc
Sahil <leviack629@gmail.com>
2023-03-08 00:02:11 +0530

    list and its methods

--- list.py
+++ list.py
@@ -1,11 +1,12 @@
 list = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+#slicing
 print(list)
 print(list[0:5])
 print(list[5:])
 print(list[:9])
 print(list[:])
 
-
+#adding
 list.append(10)
 print(list)
 
@@ -16,7 +17,38 @@ print(list)
 list.insert(0, 'a')
 print(list)
 
+
+#deleting
 list.remove(2)
 print(list)
+
 list.pop(0)
 print(list)
+
+list.clear()
+print(list)
+
+#indexing
+list = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+index = list.index(5)
+print(index)
+
+#count
+print(list.count(2))
+
+#sorting
+prime_numbers = [11, 3, 7, 5, 2]
+
+# sorting the list in ascending order
+prime_numbers.sort()
+
+print(prime_numbers)
+
+# vowels list
+vowels = ['e', 'a', 'u', 'o', 'i']
+
+# sort the vowels
+vowels.sort(reverse=True)
+
+# print vowels
+print('Sorted list (in Descending):', vowels)
