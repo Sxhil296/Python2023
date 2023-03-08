04923a3efe25793a980b22acc48e89dee364f753
Sahil <leviack629@gmail.com>
2023-03-08 23:40:59 +0530

    tuples and its methods

---
+++ tuple.py
@@ -0,0 +1,20 @@
+tuple1 = (1, 2, 3, 4)
+print(tuple1)
+
+for num in tuple1:
+    print(num)
+
+my_tuple = ("mouse", [8, 4, 6], (1, 2, 3))
+print(my_tuple)
+
+var1 = ("Sahil")
+print(type(var1))
+
+var2 = ("malik", 4)
+print(type(var2))
+
+tuple2 = (1, 2, 3, 4, 4, 5, 6, 7, 8)
+print(tuple2[4])
+
+print(tuple2[1:5])
+print(tuple2.count(4))
