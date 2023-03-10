6f59286cddd97faead7785717d59d52fc82c7f07
Sahil <leviack629@gmail.com>
2023-03-10 22:49:22 +0530

    dictionory and its methods

---
+++ dictionary.py
@@ -0,0 +1,21 @@
+# create a dictionary named capital_city
+capital_city = {'Nepal': 'Kathmandu', 'Italy': 'Rome', 'England': 'London'}
+print(capital_city)
+print(capital_city['Nepal'])
+print(capital_city.keys())
+print(capital_city.values())
+print(capital_city.clear())
+print()
+
+print(sorted(capital_city))
+numbers = {1: 'One', 2: 'Two', 3: 'Three'}
+print(numbers[3])
+numbers[4] = 'Four'
+print(numbers)
+numbers[3] = 'teen'
+print(numbers)
+print(numbers[4])
+del numbers[3] #deletes three
+print(numbers)
+print(len(numbers))
+
--- set-operation.py
+++ set-operation.py
@@ -1,5 +1,37 @@
 A = {1, 2, 3}
 B = {4, 5, 6}
+
+#union
 print('Union using |:', A | B)
 print()
 print('Union using union():', A.union(B))
+
+#intersection
+
+print()
+print('Intersection using &:', A & B)  #set()
+C = {4, 5, 6}
+D = {4, 5, 7}
+print(C & D)
+
+print()
+#difference
+E = {2, 3, 5}
+F = {1, 2, 6}
+print('Difference between A and B:', E - F)
+print(E.difference(F))
+
+
+#symmetric_difference
+G = {2, 3, 5, 7}
+H = {1, 2, 6, 7}
+print('Symmetric Difference:', G ^ H )
+print(G.symmetric_difference(H))
+
+#equality
+I = {1, 2, 3}
+J = {1, 2, 4}
+if I==J:
+    print("set I and set J are equal")
+else:
+    print("set I and set J are not equal")
