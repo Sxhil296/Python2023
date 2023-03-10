68a9ee0253ec0500c9aeedab5ca4915b28fd70c0
Sahil <leviack629@gmail.com>
2023-03-10 23:50:59 +0530

    input-output

---
+++ inp-out.py
@@ -0,0 +1,20 @@
+print("Python is a general purpose language.")
+
+print('Ramadan', 2023, 'See you soon!', sep='. ')
+
+message = "hello, how are you?"
+print(message)
+
+x = 5
+y = 10
+print('The value of x is {} and y is {}'.format(x,y))
+print(f'The value of x is {x} and y is {y}.')
+
+
+# num = input('Enter a number: ')
+# print("You entered:", num)
+# print("Data Type of num:", type(num))
+
+
+num2 = int(input('Enter a number: ' ))
+print(type(num2))
