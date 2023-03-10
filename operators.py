5469a0bf66a659abdb7064c6f9550ec415f0da21
Sahil <leviack629@gmail.com>
2023-03-11 00:22:41 +0530

    operators

---
+++ operators.py
@@ -0,0 +1,57 @@
+# a = int(input("Enter first number: "))
+# b = int(input("Enter the second number: "))
+# sum = a + b
+# difference = a - b
+# product = a * b
+# exponent = a ** b
+# division = a / b
+# floor_division = a // b
+# modulo = a % b
+# print(sum)
+
+#assignment operators
+# a = 10
+# print(a)
+# a += 2
+# print(a)
+# a -= 4
+# print(a)
+# a *= 4
+# print(a)
+# a /= 5
+# print(a)
+# a **= 2
+# print(a)
+# a %= 4
+# print(a)
+
+
+
+
+#comparison operators
+a = 10
+b =12
+print(a==b)
+print(a!=b)
+print(a>b)
+print(a<b)
+print(a>=b)
+print(a<=b)
+
+print()
+
+#logical AND operator
+print(True and True)
+print(True and False)
+print(False and False)
+
+print()
+#logical OR operator
+print(True or True)
+print(True or False)
+print(False or False)
+
+print()
+#logical NOT operator
+print(not True)
+print(not False)
