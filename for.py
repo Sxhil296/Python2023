8fe6f15b7dc1d63991de53c10792df4e1e664ade
Sahil <leviack629@gmail.com>
2023-03-11 16:48:08 +0530

    for loop

---
+++ for.py
@@ -0,0 +1,21 @@
+fruits = ['Apple', 'Mango', 'Papaya', 'Grapes', 'Banana']
+for fruit in fruits:
+    print(fruit, end=', ')
+
+
+print()
+
+values = range(6)
+for i in values:
+    print(i)
+
+
+print()
+
+digits = [0, 1, 5, 6]
+for i in digits:
+    print(i)
+    # if i == 6:
+    #     break
+else:
+    print("No more digits to print.")
