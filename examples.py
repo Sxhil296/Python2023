c14a8ecef0145638f4a0220a4b182077152f4678
Sahil <leviack629@gmail.com>
2023-03-12 10:50:49 +0530

    examples

---
+++ examples.py
@@ -0,0 +1,14 @@
+# num = int(input("Enter the number to check: "))
+# if num > 0:
+#     print(f'{num} is a positive number.')
+# else:
+#     print(f'{num} is a negative number.')
+
+
+
+
+# num2 = int(input("enter the number to check: "))
+# if num2 % 2 == 0:
+#     print(f'{num2} is an even number.')
+# else:
+#     print(f'{num2} is an odd number.')
