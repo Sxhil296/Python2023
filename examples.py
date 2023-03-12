e83bef89ded63def673a3f54941e5c111bd3b164
Sahil <leviack629@gmail.com>
2023-03-12 11:00:03 +0530

    examples

--- examples.py
+++ examples.py
@@ -12,3 +12,22 @@
 #     print(f'{num2} is an even number.')
 # else:
 #     print(f'{num2} is an odd number.')
+
+
+
+# x = 1
+# while x < 11:
+#     print(x)
+#     x += 1
+
+
+
+#print odd numbers
+# x = 0
+# while x < 20:
+#     x += 1
+#     if x % 2 == 0:
+#         continue
+#     print(x)
+
+
