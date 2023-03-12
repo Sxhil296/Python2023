33b3edab95d70f4e0f419bb2ff5da0d94ad1da1e
Sahil <leviack629@gmail.com>
2023-03-12 17:39:16 +0530

    random and math module

---
+++ math.py
@@ -0,0 +1,24 @@
+import math
+print(math.pi)
+print()
+print(math.cos(math.pi))
+print()
+print(math.exp(10))
+print()
+print(math.log10(1000))
+print()
+print(math.sinh(1))
+print()
+print(math.factorial(5))
+print()
+print(math.sqrt(9))
+x = 5.6
+print(math.ceil(x))
+y = -4
+print(math.copysign(x, y))
+print(math.fabs(x))
+print(math.floor(x))
+a=19
+b=2
+print(math.fmod(a, b))
+print(math.frexp(a))
