6e9dfec424531042ca17dac6ab098852b2bf0968
Sahil <leviack629@gmail.com>
2023-03-12 16:37:44 +0530

    module

---
+++ impModule.py
@@ -0,0 +1,19 @@
+# import exModule
+#
+# print(exModule.add(4, 5))
+
+# import math
+# print("The value of pi is: ", math.pi)
+
+# import math as m
+# print(m.pi)
+
+
+# from math import pi
+# print(pi)
+
+
+import math
+print(math.__name__)
+dir()
+
