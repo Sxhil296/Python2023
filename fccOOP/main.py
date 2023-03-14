223abe2db667582a18ef6782ffe70c66ff78e538
Sahil <leviack629@gmail.com>
2023-03-14 20:27:48 +0530

    csv

--- fccOOP/main.py
+++ fccOOP/main.py
@@ -1,3 +1,6 @@
+import csv
+
+
 class Item:
     pay_rate = 0.8 #The pay rate after 20% discount
     all = []
@@ -20,19 +23,42 @@ class Item:
     def apply_discount(self):
         self.price = self.price * self.pay_rate
 
+    @classmethod
+    def instantiate_from_csv(cls):
+        with open('items.csv', 'r') as f:
+            reader = csv.DictReader(f)
+            items = list(reader)
+
+        for item in items:
+            Item(
+                name=item.get('name'),
+                price=float(item.get('price')),
+                quantity=int(item.get('quantity')),
+            )
+
+    @staticmethod
+    def is_integer(num):
+        #We will count out the floats that are point zero
+        if isinstance(num, float):
+            #Count out the floats that are point zero
+            return num.is_integer()
+        elif isinstance(num, int):
+            return True
+        else:
+            return False
+
     def __repr__(self):
         return f"Item('{self.name}', {self.price}, {self.quantity})"
 
 
-item1 = Item("Phone", 100, 1)
-item2 = Item("Laptop", 1000, 3)
-item3 = Item("Cable", 10, 5)
-item4 = Item("Mouse", 50, 5)
-item5 = Item("Keyboard", 75, 5)
 
-print(Item.all)
+class Phone(Item): #inheritance
+    pass
 
-
+phone1 = Phone("jscPhonev10", 500, 5)
+phone1.broken_phones = 1
+phone2 = Phone("jscPhonev20", 700, 5)
+phone2.broken_phones = 1
 
 
 
