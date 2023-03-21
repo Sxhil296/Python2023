026aed72058a17e141d833db99314b669d0e288b
Sahil <leviack629@gmail.com>
2023-03-21 18:11:15 +0530

    datetime

---
+++ datetime.py
@@ -0,0 +1,14 @@
+# import datetime
+#
+# # get the current date and time
+# now = datetime.datetime.now()
+#
+# print(now)
+
+
+import datetime
+
+# get current date
+current_date = datetime.date.today()
+
+print(current_date)
